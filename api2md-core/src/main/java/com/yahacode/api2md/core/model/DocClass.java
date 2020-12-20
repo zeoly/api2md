3b630942764246ba34915c80a12865ca411e18e1
zeoly <zeoly100@163.com>
2020-12-20 23:11:34 +0800

    core model

---
+++ api2md-core/src/main/java/com/yahacode/api2md/core/model/DocClass.java
@@ -0,0 +1,14 @@
+package com.yahacode.api2md.core.model;
+
+import java.util.List;
+
+public class DocClass {
+
+    String comment;
+
+    List<String> authors;
+
+    String urlPrefix;
+
+    List<DocMethod> methods;
+}
